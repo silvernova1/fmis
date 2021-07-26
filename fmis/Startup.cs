7076b294b58f4b23fac0dec4b2a2a523270c9164
JohnArdiente <officialjla@gmail.com>
2021-07-25 19:26:52 -0700

    push updates

--- fmis/Startup.cs
+++ fmis/Startup.cs
@@ -27,52 +27,19 @@ namespace fmis
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-
-            /* services.AddRazorPages()
-                    .AddRazorPagesOptions(options => {
-                        options.RootDirectory = "/Budget";
-                    });*/
-          
-
             services.AddControllersWithViews();
-
             services.AddDbContext<fmisContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("fmisContext")));
-
            services.AddDbContext<PersonalInformationContext>(options =>
                      options.UseSqlServer(Configuration.GetConnectionString("PersonalInformationContext")));
-
             services.AddDbContext<DesignationContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("DesignationContext")));
-
             services.AddDbContext<DivisionContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("DivisionContext")));
-
-            /*var userconn = "Data Source=LAMBORGHINI\\VEEAMSQL2016;Initial Catalog=FMIS;Integrated Security=False";
-            services.AddDbContext<UserContext>(options =>
-                options.UseSqlServer(userconn));*/
-
             services.AddDbContext<SectionContext>(options =>
                   options.UseSqlServer(Configuration.GetConnectionString("SectionContext")));
 
-            /*    services.AddMvc().AddRazorPagesOptions(options => {
-                    options.Conventions.AddAreaPageRoute("Identity", "/Account/Login", "");
-                }).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
-    */
-
-            /* services.AddDbContext<PersonalInformationContext>(o =>
-                         o.UseSqlServer(Configuration.GetConnectionString("PersonalInformationContext"))
-                         .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));*/
-
-
-            /* services.AddDbContext<PersonalInformationContext>(options =>
-                     options.UseSqlServer(Configuration.GetConnectionString("PersonalInformationContext")));*/
-
-            /*services.AddScoped<PersonalInformationContext>();*/
-        }
-
-
-     
+        }    
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -84,8 +51,6 @@ namespace fmis
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-
-
             app.UseStaticFiles();
 
             app.UseRouting();
