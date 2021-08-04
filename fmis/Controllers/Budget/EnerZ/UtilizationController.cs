6a7ec8c76c664c6f08c66c675a311b5d25f71453
amalio01 <amaliojrenero@gmail.com>
2021-08-04 16:34:36 +0800

    Amalion Update

--- fmis/Controllers/Budget/EnerZ/UtilizationController.cs
+++ fmis/Controllers/Budget/EnerZ/UtilizationController.cs
@@ -7,13 +7,12 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using fmis.Data;
 using fmis.Models;
-using Syncfusion.EJ2.Grids;
-using System.Configuration;
-using System.Data.SqlClient;
+using AutoMapper;
+using System.Text.Json;
 
 namespace fmis.Controllers
 {
-    public partial class UtilizationController : Controller
+    public class UtilizationController : Controller
     {
         private readonly UtilizationContext _context;
 
@@ -21,10 +20,138 @@ namespace fmis.Controllers
         {
             _context = context;
         }
+
+        // GET: Utilization
         public async Task<IActionResult> Index()
         {
-            ViewBag.layout = "_Layout";
+            var json = JsonSerializer.Serialize(_context.Utilization.ToList());
+            ViewBag.temp = json;
             return View(await _context.Utilization.ToListAsync());
+        }
+
+        // GET: Utilization/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var utilization = await _context.Utilization
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (utilization == null)
+            {
+                return NotFound();
+            }
+
+            return View(utilization);
+        }
+
+        // GET: Utiilization/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Utilization/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Date,Dv,Pr_no,Po_no,Payee,Address,Particulars,Ors_no,Fund_source,Gross,Created_by,Date_recieved,Time_recieved,Date_released,Time_released")] Obligation obligation)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(utilization);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(utilization);
+        }
+
+        // GET: Utilization/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var utilization = await _context.Utilization.FindAsync(id);
+            if (utilization == null)
+            {
+                return NotFound();
+            }
+            return View(utilization);
+        }
+
+        // POST: Utilization/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Date,Dv,Pr_no,Po_no,Payee,Address,Particulars,Ors_no,Fund_source,Gross,Created_by,Date_recieved,Time_recieved,Date_released,Time_released")] Obligation obligation)
+        {
+            if (id !=   utilization.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(utilization);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UtilizationExists(utilization.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(utilization);
+        }
+
+        // GET: Utilization/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var utilization = await _context.Utilization
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (utilization == null)
+            {
+                return NotFound();
+            }
+
+            return View(utilization);
+        }
+
+        // POST: Utilization/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var utilization = await _context.Utilization.FindAsync(id);
+            _context.Utilization.Remove(utilization);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UtilizationExists(int id)
+        {
+            return _context.Utilization.Any(e => e.Id == id);
         }
     }
 }
