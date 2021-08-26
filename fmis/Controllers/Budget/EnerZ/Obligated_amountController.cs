3b40c8167e324dd892f806b317a7787cd0925dc4
amalio01 <amaliojrenero@gmail.com>
2021-08-26 17:47:27 +0800

    Amalio New Ups

--- fmis/Controllers/Budget/EnerZ/Obligated_amountController.cs
+++ fmis/Controllers/Budget/EnerZ/Obligated_amountController.cs
@@ -7,36 +7,26 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using fmis.Data;
 using fmis.Models;
-using AutoMapper;
-using System.Text.Json;
-using System.ComponentModel.DataAnnotations;
+
 
 namespace fmis.Controllers
 {
     public class Obligated_amountController : Controller
     {
         private readonly Obligated_amountContext _context;
+        private readonly Obligated_amountContext _dbContext;
 
-        public Obligated_amountController(Obligated_amountContext context)
+        public Obligated_amountController(Obligated_amountContext context, Obligated_amountContext dbContext)
         {
             _context = context;
+            _dbContext = dbContext;
         }
 
-        public class Obligated_amountData
-        {
-
-            public int Obligation_id { get; set; }
-            public int Expense_Title { get; set; }
-            public int Code { get; set; }
-            public float Amount { get; set; }
-          
-        }
 
         // GET: Obligated_amount
         public async Task<IActionResult> Index()
         {
-            var json = JsonSerializer.Serialize(_context.Obligated_amount.ToList());
-            ViewBag.temp = json;
+
             return View(await _context.Obligated_amount.ToListAsync());
         }
 
@@ -59,37 +49,11 @@ namespace fmis.Controllers
         }
 
         // GET: Obligated_amount/Create
-        public IActionResult Create()
+        public async Task<IActionResult> CreateAsync()
         {
+            TempData["Obligated_amount"] = await _dbContext.Obligated_amount.ToListAsync();
+            // TempData["FundSource"] = await _context.FundSource.ToListAsync();
             return View();
-        }
-
-        public ActionResult AddData(List<string[]> dataListFromTable)
-        {
-            var dataListTable = dataListFromTable;
-            return Json("Response, Data Received Successfully");
-        }
-
-        [HttpPost]
-        public IActionResult saveObligated_amount(List<Obligated_amountData> data)
-        {
-            var obligated_amounts = new List<Obligated_amount>();
-            var obligated_amount = new Obligated_amount();
-
-
-            foreach (var item in data)
-            {
-                obligated_amount.Obligation_id = item.Obligation_id;
-                obligated_amount.Expense_Title = item.Expense_Title;
-                obligated_amount.Code = item.Code;
-                obligated_amount.Amount = item.Amount;
-                obligated_amounts.Add(obligated_amount);
-            }
-
-
-            this._context.Obligated_amount.Add(obligated_amount);
-            this._context.SaveChanges();
-            return Json(data);
         }
 
         // POST: Obligated_amount/Create
@@ -97,7 +61,7 @@ namespace fmis.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Obligation_id,Expense_Title,Code,Amount")] Obligated_amount obligated_amount)
+        public async Task<IActionResult> Create([Bind("Id,Obligation_id,Expense_title,Code,Amount,Created_at,Updated_at")] Obligated_amount obligated_amount)
         {
             if (ModelState.IsValid)
             {
@@ -106,17 +70,6 @@ namespace fmis.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(obligated_amount);
-        }
-
-        [HttpPost]
-
-        public ActionResult AddObligated_amount(IEnumerable<Obligated_amount> Obligated_amountInput)
-
-        {
-
-            var p = Obligated_amountInput;
-            return null;
-
         }
 
         // GET: Obligated_amount/Edit/5
@@ -140,7 +93,7 @@ namespace fmis.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Obligation_id, Expense_Title, Code, Amount")] Obligated_amount obligated_amount)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Obligation_id,Expense_title,Code,Amount,Created_at,Updated_at")] Obligated_amount obligated_amount)
         {
             if (id != obligated_amount.Id)
             {
@@ -188,7 +141,7 @@ namespace fmis.Controllers
             return View(obligated_amount);
         }
 
-        // POST: Obligated_amount/Delete/5
+        // POST: Obligated-amount/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
--- fmis/Controllers/Budget/EnerZ/Suballotment_amountController.cs
+++ fmis/Controllers/Budget/EnerZ/Suballotment_amountController.cs
@@ -7,12 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using fmis.Data;
 using fmis.Models;
-using AutoMapper;
-using System.Text.Json;
-using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authorization;
 
 namespace fmis.Controllers
 {
+
     public class Suballotment_amountController : Controller
     {
         private readonly Suballotment_amountContext _context;
@@ -22,23 +21,13 @@ namespace fmis.Controllers
             _context = context;
         }
 
-        public class Suballotment_amountData
-        {
-
-            public int Expenses { get; set; }
-            public float Amount { get; set; }
-            public int Fund_source { get; set; }
-        }
-
-        // GET:  Suballotment_amount
+        // GET: Suallotment_amouont
         public async Task<IActionResult> Index()
         {
-            var json = JsonSerializer.Serialize(_context.Suballotment_amount.ToList());
-            ViewBag.temp = json;
             return View(await _context.Suballotment_amount.ToListAsync());
         }
 
-        // GET:  Suballotment_amount/Details/5
+        // GET: Suballotment_amount/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -48,7 +37,7 @@ namespace fmis.Controllers
 
             var suballotment_amount = await _context.Suballotment_amount
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (suballotment_amount == null)
+            if (suballotment_amount== null)
             {
                 return NotFound();
             }
@@ -56,39 +45,10 @@ namespace fmis.Controllers
             return View(suballotment_amount);
         }
 
-        // GET:  Suballotment_amount/Create
+        // GET: Suballotment_amount/Create
         public IActionResult Create()
         {
             return View();
-        }
-
-        public ActionResult AddData(List<string[]> dataListFromTable)
-        {
-            var dataListTable = dataListFromTable;
-            return Json("Response, Data Received Successfully");
-        }
-
-        [HttpPost]
-        public IActionResult saveSuballotment_amount(List<Suballotment_amountData> data)
-        {
-            var suballotment_amounts = new List<Suballotment_amount>();
-            var suballotment_amount = new Suballotment_amount();
-
-
-            foreach (var item in data)
-            {
-
-                suballotment_amount.Expenses = item.Expenses;
-                suballotment_amount.Amount = item.Amount;
-                suballotment_amount.Fund_source = item.Fund_source;
-
-                suballotment_amounts.Add(suballotment_amount);
-            }
-
-
-            this._context.Suballotment_amount.Add(suballotment_amount);
-            this._context.SaveChanges();
-            return Json(data);
         }
 
         // POST: Suballotment_amount/Create
@@ -96,26 +56,15 @@ namespace fmis.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Expenses,Amount,Fund_source")] Suballotment_amount suballotment_amount)
+        public async Task<IActionResult> Create([Bind("Id,Epenses,Amount,Fund_source")] Suballotment_amount suballotment_Amount)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(suballotment_amount);
+                _context.Add(suballotment_Amount);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(suballotment_amount);
-        }
-
-        [HttpPost]
-
-        public ActionResult AddSuballotment_amount(IEnumerable<Suballotment_amount> Suballotment_amountInput)
-
-        {
-
-            var p = Suballotment_amountInput;
-            return null;
-
+            return View(suballotment_Amount);
         }
 
         // GET: Suballotment_amount/Edit/5
@@ -134,12 +83,12 @@ namespace fmis.Controllers
             return View(suballotment_amount);
         }
 
-        // POST:  Suballotment_amount/Edit/5
+        // POST: Suballotment_amount/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Expenses,Amount,Fund_source")] Suballotment_amount suballotment_amount)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Epenses,Amount,Fund_source")] Suballotment_amount suballotment_amount)
         {
             if (id != suballotment_amount.Id)
             {
@@ -169,7 +118,7 @@ namespace fmis.Controllers
             return View(suballotment_amount);
         }
 
-        // GET:  Suballotment_amount/Delete/5
+        // GET: Suballotment_amount/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -187,7 +136,7 @@ namespace fmis.Controllers
             return View(suballotment_amount);
         }
 
-        // POST:  Suballotment_amount/Delete/5
+        // POST: Suballotment_amount/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
@@ -204,3 +153,4 @@ namespace fmis.Controllers
         }
     }
 }
+
