aa1b6a0f5fc29392d7d8ebbec064a373e1d5da3e
John Ardiente <officialjla@gmail.com>
2021-10-15 09:45:39 +0800

    Fixing Fundsource extra large modal

---
+++ fmis/Migrations/20211015011700_status.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace fmis.Migrations
+{
+    public partial class status : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "status",
+                table: "FundSourceAmount",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "token",
+                table: "FundSourceAmount",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "status",
+                table: "FundSourceAmount");
+
+            migrationBuilder.DropColumn(
+                name: "token",
+                table: "FundSourceAmount");
+        }
+    }
+}
