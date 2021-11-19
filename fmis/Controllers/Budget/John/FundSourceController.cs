b9762d54fdee4a296a57ad083269e943edea00f8
John Ardiente <officialjla@gmail.com>
2021-11-19 10:06:15 +0800

    Fixing FK Constraint in FundSource table

--- fmis/Controllers/Budget/John/FundSourceController.cs
+++ fmis/Controllers/Budget/John/FundSourceController.cs
@@ -171,36 +171,9 @@ namespace fmis.Controllers.Budget.John
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("FundSourceId,PrexcCode,FundSourceTitle,Description,FundSourceTitleCode,Respo,Budget_allotmentBudgetAllotmentId,Id")] FundSource fundSource, List<FundsourceamountData> data, int? id)
+        public async Task<IActionResult> Create([Bind("FundSourceId,PrexcCode,FundSourceTitle,Description,FundSourceTitleCode,Respo,Budget_allotmentBudgetAllotmentId,Id")] FundSource fundSource, int? id)
         {
             ViewBag.filter = new FilterSidebar("master_data", "budgetallotment");
-            var data_holder = this._MyDbContext.FundSourceAmount;
-
-            ViewBag.BudgetId = id;
-
-            foreach (var item in data)
-            {
-                if (item.Id == 0)
-                {
-
-                    var fundsourceamount = new FundSourceAmount();
-
-                    fundsourceamount.Id = item.Id;
-                    fundsourceamount.Account_title = item.Account_title;
-                    fundsourceamount.Amount = item.Amount;
-
-                    this._MyDbContext.FundSourceAmount.Update(fundsourceamount);
-                    this._MyDbContext.SaveChanges();
-                }
-                /*else
-                {
-                    data_holder.Find(item.Id).FundsId = item.FundsId;
-                    data_holder.Find(item.Id).Account_title = item.Account_title;
-                    data_holder.Find(item.Id).Amount = item.Amount;
-
-                    this._MyDbContext.SaveChanges();
-                }*/
-            }
             try
             {
                 if (ModelState.IsValid)
@@ -218,7 +191,7 @@ namespace fmis.Controllers.Budget.John
             PopulatePrexcsDropDownList(fundSource.Id);
             //return View(await _context.FundSource.Include(c => c.Budget_allotment).Where());
 
-            return View(fundSource);
+            return RedirectToAction("Fundsource", "Budget_allotments", new { id = fundSource.Budget_allotmentBudgetAllotmentId });
             /*return View("~/Views/Budget_allotments/Index.cshtml");*/
         }
 
--- fmis/Migrations/20211119012842_MyDb.Designer.cs
+++ fmis/Migrations/20211119012842_MyDb.Designer.cs
@@ -10,7 +10,7 @@ using fmis.Data;
 namespace fmis.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    [Migration("20211119010330_MyDb")]
+    [Migration("20211119012842_MyDb")]
     partial class MyDb
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
