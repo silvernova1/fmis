0bf0c32cecab372fbdd7e258b73539e0a5045cb4
Carl0Bolante <98carlo.bolante@gmail.com>
2022-01-06 11:32:16 +0800

    sub allotment delete function update

--- fmis/Migrations/20220106002330_MyDb.Designer.cs
+++ fmis/Migrations/20220106002330_MyDb.Designer.cs
@@ -10,7 +10,7 @@ using fmis.Data;
 namespace fmis.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    [Migration("20220104081855_MyDb")]
+    [Migration("20220106002330_MyDb")]
     partial class MyDb
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
