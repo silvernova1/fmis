d4cca63f96f5f668d45e48753db2be36965a7c54
Rusel T. Tayong <ruseltayong@gmail.com>
2022-01-09 10:53:43 +0800

    Add notification UI and segregate the notification for Obligation,Realignment, and Utilization

---
+++ fmis/Models/Rusel/Logs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fmis.Models.Rusel
+{
+    public class Logs
+    {
+        [Key]
+        public int LogsId { get; set; }
+        public int created_id { get; set; }
+        public string created_name { get; set; }
+        public string created_designation { get; set; }
+        public string created_division { get; set; }
+        public string created_section { get; set; }
+        public int FundSourceId { get; set; }
+        public int SubAllotmentId { get; set; }
+        public string type { get; set; } //the type are funds_realignment,sub_allotment_realignment,obligation_realignment
+        public decimal amount { get; set; }
+    }
+}
