c5df2ae7580e5fb7151e1959a86246105498e1f4
Carl0Bolante <98carlo.bolante@gmail.com>
2022-07-21 13:14:12 +0800

    Updating the After Change in Obligation to generate Id

--- fmis/Controllers/Budget/John/ObligationsController.cs
+++ fmis/Controllers/Budget/John/ObligationsController.cs
@@ -276,9 +276,8 @@ namespace fmis.Controllers
 
                 if (item.source_type == "fund_source")
                     obligation.FundSource = await _MyDbContext.FundSources.FirstOrDefaultAsync(x => x.FundSourceId == obligation.FundSourceId);
-                else
+                else 
                     obligation.SubAllotment = await _MyDbContext.SubAllotment.FirstOrDefaultAsync(x => x.SubAllotmentId == obligation.SubAllotmentId);
-
                 retObligation.Add(obligation);
             }
             return Json(retObligation.FirstOrDefault());
