2d23e081cf891a9959cd0eee9165d7617dc4e580
Carl0Bolante <98carlo.bolante@gmail.com>
2022-10-18 13:28:13 +0800

    To pull some changes

--- fmis/Models/Accounting/IndexOfPayment.cs
+++ fmis/Models/Accounting/IndexOfPayment.cs
@@ -27,12 +27,9 @@ namespace fmis.Models.Accounting
         public string InvoiceNumber { get; set; }
         public int SoNumber { get; set; }
 
-
-        [DataType(DataType.Date)]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime DvDate { get; set; }
 
-        public DateTime PeriodCover { get; set; }
+        public DateTime? PeriodCover { get; set; }
 
         public string date { get; set; }
 
